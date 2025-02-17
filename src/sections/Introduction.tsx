63e0e107cf321603fe0c91611a3613d48109976c
sleepielle <pggisselle@gmail.com>
2025-02-17 04:35:14 -0600

    feat.features: finished static section. pending animations.

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -8,8 +8,8 @@ export default function Introduction() {
                 <div className="flex justify-center">
                     <Tag>Introducing Layers</Tag>
                 </div>
-                <div className="text-4xl text-center font-medium mt-10">
-                    <span>Your creative process dserves better.</span>{" "}
+                <div className="text-4xl md:text-6xl text-center font-medium mt-10">
+                    <span>Your creative process deserves better.</span>{" "}
                     <span className="text-white/15">{text}</span>
                     <span className="text-lime-400 block">
                         That&apos;s why we built Layers.
