daccadbf32f657f07348274115fc96c856bf30fe
sleepielle <pggisselle@gmail.com>
2025-02-17 08:47:18 -0600

    feat.faq-cta-footer: finished sections. pending faq and cta animation.

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,4 +1,18 @@
-/*export default function CallToAction() {
-    return <div>Call To Action</div>;
+export default function CallToAction() {
+    return (
+        <section className="py-24">
+            <div className="overflow-x-clip p-4 flex">
+                <div className="flex flex-none gap-16 text-7xl font-medium md:text-8xl">
+                    {Array.from({ length: 10 }).map((_, i) => (
+                        <div key={i} className="flex items-center p-16">
+                            <span className="text-lime-400 text-7xl">
+                                &#10038;
+                            </span>
+                            <span>Try it for free</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
-*/
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,10 +1,33 @@
-/*const footerLinks = [
+import Image from "next/image";
+import logo from "@/assets/images/logo.svg"; // You'll need to add your logo image
+
+const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
     { href: "#", label: "Terms & Conditions" },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section>
+            <div className="container mx-auto">
+                <div className="flex flex-col lg:flex-row lg:justify-between justify-center items-center p-2">
+                    <div>
+                        <Image src={logo} alt="Logo" />
+                    </div>
+                    <div className="flex flex-col md:flex-row  items-center justify-center gap-4 p-6 pb-10">
+                        {footerLinks.map((footer) => (
+                            <a
+                                className="text-white/50"
+                                key={footer.label}
+                                href={footer.href}
+                            >
+                                {footer.label}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
-*/
